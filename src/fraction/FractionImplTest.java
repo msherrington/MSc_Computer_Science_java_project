0314bac54a831432976d781b292d83c93fe234fb
Mark Sherrington <git@marksherrington.uk>
2020-03-27 15:28:09 +0000

    more tests added

--- src/fraction/FractionImplTest.java
+++ src/fraction/FractionImplTest.java
@@ -2,47 +2,83 @@ package fraction;
 
 import org.junit.jupiter.api.*;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FractionImplTest {
 
-    private FractionImpl oneQuarter = new FractionImpl(1, 4);
-    private FractionImpl half = new FractionImpl(1,2);
-    private FractionImpl threeQuarters = new FractionImpl(3, 4);
+    private static FractionImpl minusOne;
+    private static FractionImpl zero;
+    private static FractionImpl oneQuarter;
+    private static FractionImpl half;
+    private static FractionImpl threeQuarters;
+    private static FractionImpl one;
+    private static FractionImpl two;
+
+    @BeforeAll
+    static void setUp() {
+        minusOne = new FractionImpl(-1);
+        zero = new FractionImpl(0);
+        oneQuarter = new FractionImpl(1, 4);
+        half = new FractionImpl(1,2);
+        threeQuarters = new FractionImpl(3, 4);
+        one = new FractionImpl(1);
+        two = new FractionImpl(1);
+    }
 
     // add comments to each test ???
 
     @Test
     void stringToInt() {
         assertEquals(1, FractionImpl.stringToInt("    1    "));
-        assertThrows(NumberFormatException.class, () -> {
-            FractionImpl.stringToInt("    1  1  ");
+
+        String[] testValues = {"1  1", "1.0", "abc"};
+        for (String value: testValues) {
+            assertThrows(NumberFormatException.class, () -> {
+                FractionImpl.stringToInt(value);
+            });
+        }
+    }
+
+    @Test
+    void normalise() {
+        assertEquals(1, FractionImpl.stringToInt("    1    "));
+
+        assertThrows(ArithmeticException.class, () -> {
+            FractionImpl.normalise(value);
         });
     }
 
     @Test
     void add() {
-        assertEquals(half.toString(), oneQuarter.add(oneQuarter).toString());
+        assertEquals(oneQuarter.add(oneQuarter).toString(), half.toString());
+        assertEquals(one.add(minusOne).toString(), zero.toString());
     }
 
     @Test
     void subtract() {
-        assertEquals(oneQuarter.toString(), half.subtract(oneQuarter).toString());
-        assertEquals(half.toString(), threeQuarters.subtract(oneQuarter).toString());
+        assertEquals(half.subtract(oneQuarter).toString(), oneQuarter.toString());
+        assertEquals(threeQuarters.subtract(oneQuarter).toString(), half.toString());
     }
 
     @Test
     void multiply() {
-        assertEquals(oneQuarter.toString(), half.multiply(half).toString());
+        assertEquals(half.multiply(half).toString(), oneQuarter.toString());
+        assertEquals(one.multiply(half).toString(), half.toString());
+        assertEquals(minusOne.multiply(minusOne).toString(), one.toString());
     }
 
     @Test
     void divide() {
-        assertEquals("1", half.divide(half).toString());
+        assertEquals(half.divide(half).toString(), one.toString());
+        assertEquals(oneQuarter.divide(oneQuarter).toString(), one.toString());
+        assertEquals(minusOne.divide(minusOne).toString(), one.toString());
     }
 
     @Test
     void abs() {
+        assertEquals(minusOne.abs().toString(), one.toString());
     }
 
     @Test
@@ -50,9 +86,10 @@ class FractionImplTest {
     }
 
     @Test
-    void testEquals() {
-        // accessing wrong equals method
+    void equals() {
         assertTrue(half.equals(oneQuarter.add(oneQuarter)));
+        assertTrue(threeQuarters.equals(oneQuarter.add(half)));
+        assertFalse(half.equals(oneQuarter));
     }
 
     @Test
@@ -65,10 +102,22 @@ class FractionImplTest {
 
     @Test
     void testToString() {
-        assertEquals("1", (new FractionImpl(1)).toString());
-        assertEquals("5", (new FractionImpl(5, 1)).toString());
+        assertEquals("0/1", (new FractionImpl(0)).toString());
         assertEquals("0/1", (new FractionImpl(0, 1)).toString());
-        assertEquals("5", (new FractionImpl("5/1")).toString());
+        assertEquals("0/1", (new FractionImpl("0")).toString());
         assertEquals("0/1", (new FractionImpl("0/1")).toString());
+        assertEquals("0/1", (new FractionImpl(" 0 / 1 ")).toString());
+
+        assertEquals("1", (new FractionImpl(1)).toString());
+        assertEquals("1", (new FractionImpl(1, 1)).toString());
+        assertEquals("1", (new FractionImpl("1")).toString());
+        assertEquals("1", (new FractionImpl("1/1")).toString());
+        assertEquals("1", (new FractionImpl(" 1 / 1 ")).toString());
+        assertEquals("1", (new FractionImpl("    1    /    1    ")).toString());
+
+        assertEquals("5", (new FractionImpl(5, 1)).toString());
+        assertEquals("5", (new FractionImpl("5/1")).toString());
+
+        assertNotEquals(5, (new FractionImpl(5)).toString());
     }
 }
