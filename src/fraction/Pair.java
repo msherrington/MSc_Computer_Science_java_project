03dbd00a8973ac7e2cec4988e8ac78bafbbf6785
Mark Sherrington <git@marksherrington.uk>
2020-03-28 21:44:28 +0000

    create generic Pair class

---
+++ src/fraction/Pair.java
@@ -0,0 +1,19 @@
+package fraction;
+
+public class Pair {
+    private int first;
+    private int second;
+
+    public Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public int first() {
+        return first;
+    }
+
+    public int second() {
+        return second;
+    }
+}
